62dbaa97bc7c116e08cf364427205021fd27ec1f
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-10-01 17:20:57 -0400

    Update __init__.py

--- linear/__init__.py
+++ linear/__init__.py
@@ -12,34 +12,27 @@ def compiles():
     """linear.c compiles."""
     check50.c.compile("linear.c", lcs50=True)
 
-# @check50.check(compiles)
-# def finds_28():
-#     """finds 28"""
-#     expected = "Found your number! Bingo!\n"
-#     actual = check50.run("./linear").stdout()
-#     if not re.match(expected, actual):
-#         err = Error(Mismatch("Found your number! Bingo!\n", actual))
-#         if re.match(expected[:-1], actual):
-#             err.helpers = "Did you forget a newline (\"\\n\") at the end of your printf string?"
-#         raise err
-#     check50.run("./linear").stdin("28").stdout("Found your number! Bingo!\n").exit(0)
+@check50.check(compiles)
+def finds_28():
+    """finds 28"""
+    check50.run("./linear").stdin("28").stdout("Found your number! Bingo!\n").exit(0)
         
 @check50.check(compiles)
 def finds_64():
     """finds 64"""
     check50.run("./linear").stdin("64").stdout("Found your number! Bingo!\n").exit(0)
         
-# @check50.check(compiles)
-# def finds_7():
-#     """finds 7"""
-#     check50.run("./linear").stdin("7").stdout("Found your number! Bingo!\n").exit(0)
+@check50.check(compiles)
+def finds_7():
+    """finds 7"""
+    check50.run("./linear").stdin("7").stdout("Found your number! Bingo!\n").exit(0)
         
-# @check50.check(compiles)
-# def does_not_find_31():
-#     """does not find 31"""
-#     check50.run("./linear").stdin("31").stdout("Sorry better luck next time!\n").exit(0)
+@check50.check(compiles)
+def does_not_find_31():
+    """does not find 31"""
+    check50.run("./linear").stdin("31").stdout("Sorry better luck next time!\n").exit(0)
         
-# @check50.check(compiles)
-# def does_not_find_59():
-#      """does not find 59"""
-#     check50.run("./linear").stdin("59").stdout("Sorry better luck next time!\n").exit(0)
+@check50.check(compiles)
+def does_not_find_59():
+     """does not find 59"""
+    check50.run("./linear").stdin("59").stdout("Sorry better luck next time!\n").exit(0)
