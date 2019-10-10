11006e209a3a16d4c29c424e35d42fc0dade8e5f
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-10-10 06:19:13 -0400

    Update __init__.py

--- insertion/__init__.py
+++ insertion/__init__.py
@@ -4,35 +4,15 @@ import re
 
 @check50.check()
 def exists(self):
-    """linear.c exists."""
-    check50.exists("linear.c")
+    """insertion.c exists."""
+    check50.exists("insertion.c")
 
 @check50.check(exists)
 def compiles():
-    """linear.c compiles."""
-    check50.c.compile("linear.c", lcs50=True)
+    """insertion.c compiles."""
+    check50.c.compile("insertion.c", lcs50=True)
 
 @check50.check(compiles)
-def finds_28():
-    """finds 28"""
-    check50.run("./linear").stdin("28").stdout("Found your number! Bingo!\n").exit(0)
-        
-@check50.check(compiles)
-def finds_64():
-    """finds 64"""
-    check50.run("./linear").stdin("64").stdout("Found your number! Bingo!\n").exit(0)
-        
-@check50.check(compiles)
-def finds_7():
-    """finds 7"""
-    check50.run("./linear").stdin("7").stdout("Found your number! Bingo!\n").exit(0)
-        
-@check50.check(compiles)
-def does_not_find_31():
-    """does not find 31"""
-    check50.run("./linear").stdin("31").stdout("Sorry better luck next time!\n").exit(0)
-        
-@check50.check(compiles)
-def does_not_find_59():
-    """does not find 59"""
-    check50.run("./linear").stdin("59").stdout("Sorry better luck next time!\n").exit(0)
+def sorts():
+    """sorts {1, 8, 4, 6, 0, 3, 5, 2, 7, 9}"""
+    check50.run("./insertion").stdout("0 1 2 3 4 5 6 7 8 9 \n").exit(0)
