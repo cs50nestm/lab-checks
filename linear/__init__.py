20a2510695290625ec390d097f999ecbbdcc48ef
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2021-11-17 09:05:46 -0500

    Update __init__.py

--- linear/__init__.py
+++ linear/__init__.py
@@ -13,26 +13,17 @@ def compiles():
     check50.c.compile("linear.c", lcs50=True)
 
 @check50.check(compiles)
-def finds_28():
-    """finds 28"""
-    check50.run("./linear").stdin("28").stdout("Found your number! Bingo!\n").exit(0)
+def finds_Chen():
+    """finds Chen"""
+    check50.run("./linear").stdin("Chen").stdout("Calling Chen\n").exit(0)
         
 @check50.check(compiles)
-def finds_64():
-    """finds 64"""
-    check50.run("./linear").stdin("64").stdout("Found your number! Bingo!\n").exit(0)
+def finds_Malan():
+    """finds Malan"""
+    check50.run("./linear").stdin("Malan").stdout("Calling Malan\n").exit(0)
         
 @check50.check(compiles)
-def finds_7():
-    """finds 7"""
-    check50.run("./linear").stdin("7").stdout("Found your number! Bingo!\n").exit(0)
+def does_not_finds_Tanzosh():
+    """foes_not_finds_Tanzosh"""
+    check50.run("./linear").stdin("Tanzosh").stdout("Quitting\n").exit(0)
         
-@check50.check(compiles)
-def does_not_find_31():
-    """does not find 31"""
-    check50.run("./linear").stdin("31").stdout("Sorry better luck next time!\n").exit(0)
-        
-@check50.check(compiles)
-def does_not_find_59():
-    """does not find 59"""
-    check50.run("./linear").stdin("59").stdout("Sorry better luck next time!\n").exit(0)
